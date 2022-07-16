532b30d4fa383b994a528162623809cab82b5e33
Luis Carlos <crluis521@gmail.com>
2022-07-16 15:17:41 -0300

    primeira implementação do js

---
+++ models/user.js
@@ -0,0 +1,45 @@
+class Usuario {
+    id; //(automático json-server)
+    tipo;
+    nome;
+    dataNascimento; // salvar como objeto Date e não como string '10/05/1990' por exemplo
+    email;
+    senha;
+    candidaturas = []; // lista de Candidatura
+
+    constructor(tipo, nome, dataNascimento, email, senha, candidaturas){
+        this.tipo = tipo;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+        this.senha = senha;
+        this.candidaturas = candidaturas;
+    }
+  }
+
+  class Candidatura {
+    idVaga;
+    idCandidato;
+    reprovado; // true or false
+
+    constructor(idVaga,idCandidato, reprovado){
+        this.idVaga = idVaga;
+        this.idCandidato = idCandidato;
+        this.reprovado = reprovado;
+    }
+  }
+
+  class Vaga {
+    id; //(automático json-server)
+    titulo;
+    descricao;
+    remuneracao; //(salvar no formato: R$ 3200.50)
+    candidatos = []; // lista de Trabalhadores candidatados na vaga
+
+    constructor(titulo, descricao, remuneracao, candidatos){
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.remuneracao = remuneracao;
+        this.candidatos = candidatos;
+    }
+  }
