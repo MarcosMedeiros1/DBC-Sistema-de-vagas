52dce3d555920d55c4d3f9a1931618619b6b3f00
Marcos Medeiros <mmarcos787@gmail.com>
2022-07-18 10:43:34 -0300

    merge

--- teste.js
+++
@@ -1,9 +0,0 @@
-// let teste = 'Luis Carlos'
-// contadorNumeros = 0;
-// numeros='1234567890'
-
-// for (let i = 0 < i.teste.length; i++){
-//     if(numeros.indexOf(teste[i]) !== -1){
-//         contadorNumeros +=1
-//     }
-// }
